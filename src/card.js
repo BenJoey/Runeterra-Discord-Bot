316a5542a1cca133f55407cf3cd0bd7ab26ad31b
Bence <bauerbence5@gmail.com>
2020-12-24 21:50:03 +0100

    card.js fancy

--- src/card.js
+++ src/card.js
@@ -2,21 +2,16 @@ const Discord = require('discord.js');
 const data = require('./../data/set1.json').concat(require('./../data/set2.json')).concat(require('./../data/set3.json'));
 
 module.exports = {
-	name: 'card',
-	description: 'Find card by name',
-	execute(message, args) {
+    name: 'card',
+    description: 'Find card image by name',
+    execute(message, args) {
 
         let cards = findCardByName(args.join(' ').toLowerCase());
-        if(cards.length == 0) return;
+        if(cards.length == 0) return message.channel.send("This card does not exist!");
 
         cards.forEach(function(card){
             let embed = new Discord.MessageEmbed();
-            embed.setImage(card.assets[0].gameAbsolutePath);
-            /*embed.addField("Name", card.name, true);
-            embed.addField("Cost", card.cost, true);
-            embed.addField("Type", card.type, true);
-            embed.addField("Region", card.region, true);
-            console.log(card.assets[0].gameAbsolutePath)*/
+            embed.setImage(card);
             message.channel.send(embed);
         });
     }
@@ -24,10 +19,10 @@ module.exports = {
 
 function findCardByName(name){
     let toRet = [];
-	for (var i = 0; i < data.length; i++) {
-        if (data[i].name.toLowerCase() == name) {
-		   toRet.push(data[i]);
-       }
-	}
-	return toRet;
+    for (const card of data) {
+        if (card.name.toLowerCase() == name) {
+           toRet.push(card.assets[0].gameAbsolutePath);
+        }
+    }
+    return toRet;
 }
