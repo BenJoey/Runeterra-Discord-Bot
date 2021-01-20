8f4f3f75b39b6ade732e41a7a3207d40373fbef4
Bence <bauerbence5@gmail.com>
2021-01-20 19:09:00 +0100

    Refactored deck.js

--- src/deck.js
+++ src/deck.js
@@ -6,93 +6,104 @@ const data = require('./../data/set1.json').concat(require('./../data/set2.json'
 //const globals = require('./../data/keywords.json');
 
 module.exports = {
-	name: 'deck',
-	description: 'Display deck from deck code',
-	execute(message, args) {
+    name: 'deck',
+    description: 'Display deck from deck code',
+    execute(message, args) {
         if (!args.length) {
-			return message.channel.send(`Please provide a deck code, ${message.author}!`);
-		}
+            return message.channel.send(`Please provide a deck code, ${message.author}!`);
+        }
 
-		let deck = null;
-		let formattedDeck = [];
-		let isSingleton = true;
+        let deck = null;
+        let formattedDeck = [];
+        let isSingleton = true;
 
-		try {
-			//TODO: Create own deck decoder
-			deck = DeckEncoder.decode(args[0]);
-		} catch(err) {
-			return message.channel.send(`Please provide a valid deck code, ${message.author}!`);
-		}
+        try {
+            //TODO: Create own deck decoder
+            deck = DeckEncoder.decode(args[0]);
+        } catch(err) {
+            return message.channel.send(`Please provide a valid deck code, ${message.author}!`);
+        }
 
-		for (const decodedCard of deck) {
-			/*if (decodedCard.code.includes("undefined")) {
-				decodedCard.code = decodedCard.code.replace("undefined", "MT");
-			}*/
+        for (const decodedCard of deck) {
+            /*if (decodedCard.code.includes("undefined")) {
+                decodedCard.code = decodedCard.code.replace("undefined", "MT");
+            }*/
 
-			let card = data.find(elem => elem.cardCode == decodedCard.code);
-			if(card == undefined) return message.channel.send(`Please provide a valid deck code, ${message.author}!`);
-			formattedDeck.push({cost: card.cost, name: card.name, count: decodedCard.count, type: card.type, rarity: card.rarity, region: card.region});
-			isSingleton = isSingleton && decodedCard.count == 1;
-		}
+            let card = data.find(elem => elem.cardCode == decodedCard.code);
+            if(card == undefined) return message.channel.send(`Please provide a valid deck code, ${message.author}!`);
+            formattedDeck.push({cost: card.cost,
+                                name: card.name,
+                                count: decodedCard.count,
+                                type: getCardType(card.rarity, card.type),
+                                rarity: card.rarity,
+                                region: card.region });
+            
+            isSingleton = isSingleton && decodedCard.count == 1;
+        }
 
-		formattedDeck.sort(sortByCost);
-		let detailsText = createDetailsText(formattedDeck, isSingleton);
+        formattedDeck.sort(sortByCost);
 
-		let embed = new Discord.MessageEmbed();
-		embed.addField("Deck Details", detailsText);
-		if(formattedDeck.filter(a => a.rarity == "Champion").length > 0) {
-			embed.addField("Champions", createDeckText(formattedDeck.filter(a => a.rarity == "Champion"), isSingleton), true);
-		}
-		if(formattedDeck.filter(a => a.type == "Unit" && a.rarity != "Champion").length > 0) {
-			embed.addField("Units", createDeckText(formattedDeck.filter(a => a.type == "Unit" && a.rarity != "Champion"), isSingleton), true);
-		}
-		if(formattedDeck.filter(a => a.type == "Spell").length > 0) {
-			embed.addField("Spells", createDeckText(formattedDeck.filter(a => a.type == "Spell"), isSingleton), true);
-		}
-		if(formattedDeck.filter(a => a.type == "Landmark").length > 0) {
-			embed.addField("Landmarks", createDeckText(formattedDeck.filter(a => a.type == "Landmark"), isSingleton));
-		}
-		message.channel.send(embed);
-	},
+        let embed = new Discord.MessageEmbed();
+        embed.addField("Deck Details", createDetailsText(formattedDeck, isSingleton));
+        
+        let cardTypes = [
+            {name: "Champion", isInline: true},
+            {name: "Unit", isInline: true},
+            {name: "Spell", isInline: true},
+            {name: "Landmark", isInline: false}
+        ];
+
+        cardTypes.forEach(function(type) {
+            let filteredDeck = formattedDeck.filter(a => a.type == type.name);
+            if(filteredDeck.length > 0) embed.addField(type.name + "s", createDeckText(filteredDeck, isSingleton), type.isInline);
+        });
+
+        return message.channel.send(embed);
+    },
 };
 
 function createDetailsText(deck, isSingleton) {
-	let details = [];
-	details.push("**Regions:** " + createRegionsText(deck.map(a => a = a.region)));
-	let deckcost = 0;
-	for(const card of deck) {
-		deckcost += getCardCost(card.rarity.toLowerCase()) * card.count;
-	}
-	details.push("**Deck cost:** " + deckcost + " Shard");
-	details.push("**Deckbuilding rule:** " + (isSingleton ? "Singleton" : "Normal"));
-	return details.join('\n');
+    let details = [];
+    details.push("**Regions:** " + createRegionsText(deck.map(a => a = a.region)));
+    let deckcost = 0;
+    for(const card of deck) {
+        deckcost += getCardCraftingCost(card.rarity.toLowerCase()) * card.count;
+    }
+    details.push("**Deck cost:** " + deckcost + " Shard");
+    details.push("**Deckbuilding rule:** " + (isSingleton ? "Singleton" : "Normal"));
+    return details.join('\n');
 }
 
 function createRegionsText(deckRegions) {
-	let regions = deckRegions.filter(function(value, index, self) {
-		return self.indexOf(value) === index;
-	});
-	return regions.join(', ');
-}
-
-function sortByCost(a,b) {
-	var x = a.cost; var y = b.cost;
-	return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    let regions = deckRegions.filter(function(value, index, self) {
+        return self.indexOf(value) === index;
+    });
+    return regions.join(', ');
 }
 
 function createDeckText(deckArray, isSingleton) {
-	let toRet = [];
-	deckArray.forEach(function(elem){
-		toRet.push(elem.name + (isSingleton ? "" : " (x" + elem.count + ")"));
-	});
-	return toRet.join('\n');
+    let toRet = [];
+    deckArray.forEach(function(elem){
+        toRet.push(elem.name + (isSingleton ? "" : " (x" + elem.count + ")"));
+    });
+    return toRet.join('\n');
 }
 
-function getCardCost(rarity) {
+function sortByCost(a,b) {
+    var x = a.cost; var y = b.cost;
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+}
+
+function getCardCraftingCost(rarity) {
     switch(rarity){
         case "common": return 100;
         case "rare": return 300;
         case "epic": return 1200;
         case "champion": return 3000;
     }
+}
+
+function getCardType(rarity, type){
+    if(rarity == "Champion" && type == "Unit") return "Champion";
+    return type;
 }
--- src/message.js
+++ src/message.js
@@ -1,14 +1,14 @@
 module.exports = {
-	name: 'message',
-	description: 'Handle received message',
-	execute(message, client) {
-		args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
-		const command = args.shift().toLowerCase();
-		if (command == "help") args = client.config;
-		try{
-			client.commands.get(command).execute(message, args);
-		} catch (err){
-			return;
-		}
-	}
+    name: 'message',
+    description: 'Handle received message',
+    execute(message, client) {
+        args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+        if (command == "help") args = client.config;
+        try{
+            client.commands.get(command).execute(message, args);
+        } catch (err) {
+            return message.channel.send("Invalid command!");
+        }
+    }
 };
